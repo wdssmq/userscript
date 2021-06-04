55139ace41eed9a7cbad08787212b747f66af500
沉冰浮水 <wdssmq@qq.com>
2021-06-04 20:34:28 +0800

    update QQ\GroupMemberRemove.user.js

--- QQ/GroupMemberRemove.user.js
+++ QQ/GroupMemberRemove.user.js
@@ -12,33 +12,38 @@
 // @link        https://greasyfork.org/zh-CN/scripts/26812
 // ----------------------------
 // @include     https://qun.qq.com/member.html*
-// @include     http://qun.qq.com/member.html*
 // @grant       none
 // ==/UserScript==
-function $n(e) {
-  return document.querySelector(e);
-}
-function $na(e) {
-  return document.querySelectorAll(e);
-}
 
-function fnRun() {
-  if (!$n(".del-member.disabled"))
-    return false;
-  $n(".del-member.disabled").removeAttribute("disabled");
-  $n(".del-member.disabled").className = "del-member";
-
-  var mbList = $na('.mb .check-input');
-  for (var x = 0; x < 20; x++) {
-    mbList[x].setAttribute('checked', 'checked');
+(function () {
+  'use strict';
+  function $n(e) {
+    return document.querySelector(e);
   }
-  fnRun();
-}
-window.addEventListener('scroll', function () {
-  fnRun();
-});
-$n('.select-result .submit').addEventListener('click', function () {
-  t = setTimeout(function () {
+  function $na(e) {
+    return document.querySelectorAll(e);
+  }
+
+  function fnRun() {
+    if (!$n(".del-member.disabled"))
+      return false;
+    $n(".del-member.disabled").removeAttribute("disabled");
+    $n(".del-member.disabled").className = "del-member";
+
+    var mbList = $na('.mb .check-input');
+    for (var x = 0; x < 20; x++) {
+      mbList[x].setAttribute('checked', 'checked');
+    }
     fnRun();
-  }, 900);
-}, false);
+  }
+  window.addEventListener('scroll', function () {
+    fnRun();
+  });
+  $n('.select-result .submit').addEventListener('click', function () {
+    t = setTimeout(function () {
+      fnRun();
+    }, 900);
+  }, false);
+})();
+
+
