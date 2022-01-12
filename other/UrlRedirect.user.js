9201919234352fc0f84087baae54a8ecd92e1d1e
wdssmq <wdssmq@qq.com>
2022-01-12 19:45:42 +0800

    add other\UrlRedirect.user.js

---
+++ other/UrlRedirect.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         「水水」链接跳转
+// @namespace    https://www.wdssmq.com/
+// @version      0.1
+// @author       沉冰浮水
+// @description  跳转到正确的链接
+// @match        https://jump.bdimg.com/f?kw=*
+// @icon         https://www.google.com/s2/favicons?domain=bdimg.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  // 百度贴吧的各种链接统一
+  (() => {
+    const arrHostList = [
+      "jump.bdimg.com"
+    ];
+    const curHost = window.location.host;
+    if (arrHostList.includes(curHost)) {
+      const newUrl = window.location.href.replace(curHost, "tieba.baidu.com");
+      window.location.href = newUrl;
+    }
+  })();
+
+})();
