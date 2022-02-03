f7ee134826b0fe51f9afca3e0859b8dbd77c016e
wdssmq <wdssmq@qq.com>
2022-02-03 16:17:10 +0800

    feat. B 币领取提醒 - bilibili/

--- bilibili/later.user.js
+++ bilibili/later.user.js
@@ -8,15 +8,21 @@
 // @include      https://www.bilibili.com/*
 // @include      https://t.bilibili.com/*
 // @include      https://manga.bilibili.com/account-center*
+// @include      https://account.bilibili.com/account/big/myPackage
 // @icon         https://www.bilibili.com/favicon.ico
 // @run-at       document-end
 // @grant        GM_setClipboard
+// @grant        GM_notification
+// @grant        GM.openInTab
 
 // ==/UserScript==
 /* jshint esversion:6 */
 (function () {
   "use strict";
+  // 基础函数或变量
+  const curUrl = window.location.href;
   const $ = window.$ || unsafeWindow.$;
+  const curDate = new Date();
   const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
   const _log = (...args) => console.log('[bilibili-helper]', ...args);
   const _warn = (...args) => console.warn('[bilibili-helper]', ...args);
@@ -27,6 +33,91 @@
   function $na(e) {
     return document.querySelectorAll(e);
   }
+
+  // cookie 封装
+  const ckeObj = {
+    setItem: function (key, value) {
+      const Days = 137;
+      const exp = new Date();
+      exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
+      document.cookie = key + "=" + encodeURIComponent(value) + ";path=/;domain=.bilibili.com;expires=" + exp.toGMTString();
+    },
+    getItem: function (key, def = "") {
+      const reg = new RegExp("(^| )" + key + "=([^;]*)(;|$)");
+      const arr = document.cookie.match(reg);
+      if (arr) {
+        return arr[2];
+      }
+      return def;
+    }
+  };
+
+  // B 币领取提醒
+  (() => {
+    const ckeName = "bilibili-helper-bcoin-lstMonth";
+    const curMonth = curDate.getMonth() + 1;
+    const lstMonth = ckeObj.getItem(ckeName, 0);
+    const bcoinUrl = "https://account.bilibili.com/account/big/myPackage";
+
+    // 元素变化监听
+    const fnElChange = (el, fn = () => { }) => {
+      const observer = new MutationObserver((mutationRecord, mutationObserver) => {
+        _log('body attributes changed!!!'); // body attributes changed!!!
+        _log('mutationRecord = ', mutationRecord); // [MutationRecord]
+        _log('mutationObserver === observer', mutationObserver === observer); // true
+        fn(mutationRecord, mutationObserver);
+        mutationObserver.disconnect();
+      });
+      observer.observe(el, {
+        // attributes: false,
+        // attributeFilter: ["class"],
+        childList: true,
+        // characterData: false,
+        subtree: true,
+      });
+    }
+
+    // 通知事件封装
+    const notify = (title, body) => {
+      GM_notification({
+        title: title,
+        text: body,
+        timeout: 0,
+        onclick: () => {
+          // window.location.href = bcoinUrl;
+          GM.openInTab(bcoinUrl, false);
+        }
+      });
+    }
+
+    // 判断是否已经领取过
+    const fnCheckByDOM = () => {
+      const $bcoin = $n(".bcoin-wrapper");
+      _log("---");
+      // $bcoin && _log($bcoin.innerHTML);
+      if ($bcoin && $bcoin.innerText.includes("本月已领")) {
+        ckeObj.setItem(ckeName, curMonth);
+        return true;
+      } else {
+        fnElChange($n("#app"), fnCheckByDOM);
+      }
+      return false;
+    }
+
+    // _log($n("body").innerHTML);
+    // _log(lstMonth, curMonth);
+
+    // 对比 ls 数据
+    if (lstMonth != curMonth) {
+      _log(curUrl, bcoinUrl);
+      if (curUrl.indexOf(bcoinUrl) > -1) {
+        fnCheckByDOM();
+      } else {
+        notify("B 币领取提醒", "点击查看 B 币领取情况");
+      }
+    }
+  })();
+
   // 番剧链接改为我的追番
   (() => {
     document.addEventListener(
