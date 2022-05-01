ded69e5a98ab12e523ece845b8abcf5aaa7641bd
wdssmq <wdssmq@qq.com>
2022-05-01 15:33:09 +0800

    add. 基于 rollup 的空项目；

---
+++ empty_def/src/__info.js
@@ -0,0 +1,21 @@
+const gm_banner = `
+// ==UserScript==
+// @name         New Userscript
+// @namespace    https://www.wdssmq.com/
+// @version      0.1
+// @author       沉冰浮水
+// @description  try to take over the world!
+// @link     ----------------------------
+// @link     https://github.com/wdssmq/userscript
+// @link     https://afdian.net/@wdssmq
+// @link     https://greasyfork.org/zh-CN/users/6865-wdssmq
+// @link     ----------------------------
+// @match        <$URL$>
+// @grant        none
+// ==/UserScript==
+/* jshint esversion: 6 */
+`.trim();
+
+const gm_name = "empty_def";
+
+export { gm_banner, gm_name };
---
+++ empty_def/src/_base.js
@@ -0,0 +1,35 @@
+import { gm_name } from "./__info";
+
+// 初始常量或函数
+const curUrl = window.location.href;
+const curDate = new Date();
+// ---------------------------------------------------
+const _curUrl = () => { return window.location.href; };
+const _curDate = () => { return new Date(); };
+const _sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+// ---------------------------------------------------
+const _log = (...args) => console.log(`${gm_name}\n`, ...args);
+const _warn = (...args) => console.warn(`${gm_name}\n`, ...args);
+const _error = (...args) => console.error(`${gm_name}\n`, ...args);
+// ---------------------------------------------------
+const $ = window.$ || unsafeWindow.$;
+function $n(e) {
+  return document.querySelector(e);
+}
+function $na(e) {
+  return document.querySelectorAll(e);
+}
+// ---------------------------------------------------
+export {
+  curUrl,
+  // curDate,
+  _curUrl,
+  // _curDate,
+  // _sleep,
+  _log,
+  // _warn,
+  // _error,
+  // $,
+  $n,
+  $na,
+};
