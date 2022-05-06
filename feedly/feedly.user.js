56410c8720529788fc65d89818121ea4e9303444
wdssmq <wdssmq@qq.com>
2022-05-06 20:11:25 +0800

    build. feedly.user.js；

--- feedly/feedly.user.js
+++ feedly/feedly.user.js
@@ -1,8 +1,9 @@
+
 // ==UserScript==
 // @name         「Feedly」- 中键标记已读 + 收藏导出为*.url
 // @namespace    https://www.wdssmq.com/
+// @version      0.3.6
 // @author       沉冰浮水
-// @version      0.3.5
 // @description  新标签页打开条目时自动标记为已读，收藏计数
 // ----------------------------
 // @raw    https://github.com/wdssmq/userscript/tree/master/feedly
@@ -18,43 +19,39 @@
 // ==/UserScript==
 /* jshint esversion: 6 */
 (function () {
-  "use strict";
-  // 基础函数或变量
-  const curUrl = window.location.href;
+  'use strict';
+
+  const gm_name = "feedly";
+
   const curDate = new Date();
-  // const $ = window.$ || unsafeWindow.$;
+  // ---------------------------------------------------
   const _curUrl = () => { return window.location.href; };
-  const _sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-  const _log = (...args) => console.log("[Feedly]\n", ...args);
-  const _warn = (...args) => console.warn("[Feedly]\n", ...args);
-  const _error = (...args) => console.error("[Feedly]\n", ...args);
+  // ---------------------------------------------------
+  const _log = (...args) => console.log(`[${gm_name}]\n`, ...args);
+  // ---------------------------------------------------
+  // const $ = window.$ || unsafeWindow.$;
   function $n(e) {
     return document.querySelector(e);
   }
   function $na(e) {
     return document.querySelectorAll(e);
   }
-
-  // 附加函数
-  function fnRmovoDOM(el) {
-    el.parentNode.removeChild(el);
-  }
-  function fnFindDomUp(el, selector, up = 1) {
-    // _log("fnFindDomUp", el, selector, up);
-    const elParent = el.parentNode;
-    if (selector.indexOf(".") == 0 && elParent.className.indexOf(selector.split(".")[1]) > -1) {
-      return elParent;
-    }
-    const elFind = elParent.parentNode.querySelector(selector);
-    if (elFind) {
-      return elFind;
-    }
-    if (up > 1) {
-      return fnFindDomUp(elParent, selector, up - 1);
-    } else {
-      return null;
-    }
-  }
+  // ---------------------------------------------------
+  const fnElChange = (el, fn = () => { }) => {
+    const observer = new MutationObserver((mutationRecord, mutationObserver) => {
+      // _log('mutationRecord = ', mutationRecord);
+      // _log('mutationObserver === observer', mutationObserver === observer);
+      fn(mutationRecord, mutationObserver);
+      // mutationObserver.disconnect();
+    });
+    observer.observe(el, {
+      // attributes: false,
+      // attributeFilter: ["class"],
+      childList: true,
+      // characterData: false,
+      subtree: true,
+    });
+  };
 
   // localStorage 封装
   const lsObj = {
@@ -70,60 +67,209 @@
     },
   };
 
+  // 数据读写封装
   const gob = {
     // 非 ls 字段
-    loaded: false,
-    curStars: 0,
+    _loaded: false,
+    _curStars: 0,
     // ls 字段
-    bolReset: false,
-    lstStars: 0,
-    diffStars: { decr: 0, incr: 0 },
-    // 读取
-    load: function () {
-      if (this.loaded) {
-        return;
-      }
-      this.loaded = true;
-      this.lstStars = lsObj.getItem("lstStars", 0);
+    data: {
+      bolReset: false,
+      lstStars: 0,
+      diffStars: { decr: 0, incr: 0 },
       // decrease 减少
       // increase 增加
-      this.diffStars = lsObj.getItem("diffStars", this.diffStars);
-      this.bolReset = lsObj.getItem("bolReset", false);
+    },
+    // 读取
+    load: function () {
+      if (this._loaded) {
+        return;
+      }
+      this._loaded = true;
+      this.data = lsObj.getItem("feedly_bld_data", this.data);
       _log("load", gob);
     },
     // 保存
     save: function () {
-      lsObj.setItem("lstStars", this.lstStars);
-      lsObj.setItem("diffStars", this.diffStars);
-      lsObj.setItem("bolReset", this.bolReset);
+      lsObj.setItem("feedly_bld_data", this.data);
       _log("save", gob);
     },
   };
 
-  // 拿回订阅源地址
-  // 绑定监听事件到 div#box 上
-  $n("#box").addEventListener("mouseup", function (event) {
-    // 输出触发事件的元素
-    // _log("105",event.target);
-    // 根据内容判断是否执行相应操作
-    const elText = event.target.innerHTML;
-    if (
-      // elText.indexOf("Feed not found") > -1 ||
-      elText.indexOf("Wrong feed URL") > -1
-    ) {
-      // 内部再输出一次确定判断条件正确
-      console.log(event.target);
-      // 拿到解码后的订阅源地址
-      const curUrl = ((url) => {
-        return url.replace("https://feedly.com/i/subscription/feed/", "");
-      })(decodeURIComponent(curUrl));
-      // 输出到页面中
-      $n("#feedlyPageFX h2").insertAdjacentHTML(
-        "beforeend",
-        `<div class="sub">${curUrl}</div>`
-      );
+  // 判断当前地址是否是收藏页
+  const fnCheckUrl = () => {
+    if ("https://feedly.com/i/saved" === _curUrl()) {
+      return true;
     }
-  }, false);
+    return false;
+  };
+
+  // 当前星标数获取
+  function fnLaterGetItems(obj) {
+    const $listWrap = $n("div.list-entries");
+    if ($listWrap) {
+      obj.$list = $listWrap.querySelectorAll("div.content a");
+      return obj.$list.length;
+    }
+    return 0;
+  }
+
+  // 收藏数 View
+  function fnLaterViewStars() {
+    const objTime = {
+      time: 0,
+      rem: 0,
+    };
+    if (fnCheckControl(gob.data.diffStars, objTime) === "lock") {
+      $n(".list-entries").style.backgroundColor = "#ddd";
+    }
+    gob._curStars = fnLaterGetItems(gob);
+    const strText = `Read later（${gob._curStars} 丨 -${gob.data.diffStars.decr} 丨 +${gob.data.diffStars.incr}）（${objTime.time} - ${objTime.rem}）`;
+    $n("h1 #header-title").innerHTML = strText;
+    if ($n("header.header h2")) {
+      $n("header.header h2").innerHTML = strText;
+    }
+    $n("#header-title").innerHTML = strText;
+  }
+
+  // 滚动条滚动时触发
+  function fnLaterOnScroll() {
+    if (!fnCheckUrl()) {
+      return;
+    }
+    fnLaterViewStars();
+    fnColorStars();
+    // 全部条目加载后执行
+    if ($n(".list-entries > h2")) {
+      !gob._loaded && _log("fnLaterOnScroll", "页面加载完成");
+      fnLaterControl();
+    } else {
+      _log("fnLaterOnScroll", "页面加载中");
+    }
+  }
+
+  // 星标部分入口函数
+  function fnLaterMain(record, observer) {
+    if (!fnCheckUrl()) {
+      return;
+    }
+    // 随机直接返回
+    if (Math.random() > 0.4) {
+      return;
+    }
+    gob._curStars = fnLaterGetItems(gob);
+    if (gob._curStars === 0) {
+      return;
+    }
+    // observer.disconnect();
+    // 绑定事件
+    if ($n("#feedlyFrame") && $n("#feedlyFrame").dataset.addEL !== "done") {
+      fnLaterOnScroll();
+      $n("#feedlyFrame").addEventListener("scroll", fnLaterOnScroll);
+      $n("#feedlyFrame").dataset.addEL = "done";
+      _log("fnLaterMain", "绑定滚动监听");
+    }
+  }
+
+  fnElChange($n("#box"), fnLaterMain);
+
+  const curTime = Math.floor(curDate.getTime() / 1000);
+  const curHours = Math.floor(curTime / 3600);
+  // const cur4Hours = Math.floor(curTime / (60 * 60 * 4));
+  const cur4Minutes = Math.floor(curTime / 240);
+
+  const fnCheckControl = (diff, objSec = {}) => {
+    const iTime = curHours;
+    objSec.time = iTime;
+    objSec.rem = iTime % 4;
+    // 累计已读少于 17 或者累计新增大于累计已读
+    if (diff.decr < 17 || diff.incr - diff.decr > 4) {
+      return "default";
+    }
+    // 每 4 小时且累计已读大于累计新增，或者累计新增过大（一般为初始运行时）
+    if ((iTime % 4 === 0 && diff.decr - diff.incr > 4) || gob._curStars - diff.incr < 4) {
+      return "reset";
+    }
+    return "lock";
+  };
+
+  // 星标变动控制
+  function fnLaterControl() {
+    if (gob._loaded) {
+      return;
+    }
+    gob.load();
+
+    // 解锁重置判断
+    if (gob.data.bolReset) {
+      gob.data.diffStars.decr = 0;
+      gob.data.diffStars.incr = 0;
+    }
+
+    // 星标变化计数
+    const diff = gob._curStars - gob.data.lstStars;
+
+    // 大于上一次记录
+    if (diff > 0) {
+      // 新增星标计数
+      gob.data.diffStars.incr += Math.abs(diff);
+    } else {
+      // 已读星标计数
+      gob.data.diffStars.decr += Math.abs(diff);
+    }
+
+    // 记录变量原始值
+    const strReset = gob.data.bolReset.toString();
+
+    // _log("fnLaterControl", strReset);
+
+    gob.data.bolReset = ((diff) => {
+      if (fnCheckControl(diff) === "reset") {
+        return true;
+      }
+      return false;
+    })(gob.data.diffStars);
+
+    gob.data.lstStars = gob._curStars;
+
+    // 更新 localStorage 存储
+    if (diff !== 0 || strReset !== gob.data.bolReset.toString()) {
+      gob.save();
+    }
+  }
+
+  // 按规则给星标条目着色
+  function fnColorStars() {
+    // _log("fnColorStars", curTime, cur4Minutes);
+
+    const $stars = $na("div.content a");
+    let pickCount = 0;
+    [].forEach.call($stars, function ($e, i) {
+      // _log("fnColorStars", $e, i);
+      // _log("fnColorStars", "==============================");
+
+      const href = $e.href;
+      const hash = parseInt(href.replace(/\D/g, ""));
+
+      // _log("fnColorStars", href, hash);
+
+      const intNum = parseInt(hash + cur4Minutes + i);
+      // const intNum = parseInt(i + cur4Minutes);
+
+      // _log("fnColorStars", intNum, intNum % 4);
+
+      if (intNum % 4 === 0) {
+        pickCount++;
+        $e.parentNode.parentNode.style.backgroundColor = "#ddd";
+      } else {
+        $e.parentNode.parentNode.style.backgroundColor = "transparent";
+        if (fnCheckControl(gob.data.diffStars) === "lock" || pickCount > 13) {
+          // console.log($e.parentNode.parentNode.classList);
+          $e.parentNode.parentNode.style.backgroundColor = "#666";
+        }
+      }
+    });
+  }
 
   // 星标文章导出为 *.url 文件
   function fnMKShell($list) {
@@ -161,163 +307,33 @@
     if (event.target.innerHTML.indexOf("Read later") > -1) {
       const $el = event.target;
       console.log($el);
-      fnOnScroll();
       GM_setClipboard(fnMKShell($na("div.content a")));
     }
   }, false);
 
-  // 加载完成后执行
-  window.onload = function () {
-    $n("#box").addEventListener("mouseover", fnOnScroll, false);
-    fnOnScroll();
-  };
-
-  // 星标计数触发和更新
-  async function fnOnScroll(event = null) {
-    // 判断页面地址
-    if ("https://feedly.com/i/saved" !== _curUrl()) {
-      return;
+  // 拿回订阅源地址
+  // 绑定监听事件到 div#box 上
+  $n("#box").addEventListener("mouseup", function (event) {
+    // 输出触发事件的元素
+    // 根据内容判断是否执行相应操作
+    const elText = event.target.innerHTML;
+    if (
+      // elText.indexOf("Feed not found") > -1 ||
+      elText.indexOf("Wrong feed URL") > -1
+    ) {
+      // 内部再输出一次确定判断条件正确
+      console.log(event.target);
+      // 拿到解码后的订阅源地址
+      const curUrl = ((url) => {
+        return url.replace("https://feedly.com/i/subscription/feed/", "");
+      })(decodeURIComponent(curUrl));
+      // 输出到页面中
+      $n("#feedlyPageFX h2").insertAdjacentHTML(
+        "beforeend",
+        `<div class="sub">${curUrl}</div>`
+      );
     }
-    if (event !== null && $n("#box").dataset.OnScroll === "remove") {
-      _log("fnOnScroll", $n("#box").dataset.OnScroll);
-      return;
-    } else {
-      $n("#box").dataset.OnScroll = "remove";
-      $n("#box").removeEventListener("mouseover", fnOnScroll, false);
-    }
-    await _sleep(3000);
-    // 全部条目加载后执行
-    if ($n(".list-entries > h2")) {
-      gob.curStars = $na("div.content a").length;
-      fnLaterControl();
-      fnViewStars();
-      fnColorStars();
-      _log("fnOnScroll", "星标计数触发");
-    } else {
-      fnOnScroll();
-      _log("fnOnScroll", "页面加载中");
-    }
-    // 滚动条滚动时触发
-    if ($n("#feedlyFrame") && $n("#feedlyFrame").dataset.addEL !== "done") {
-      $n("#feedlyFrame").dataset.addEL = "done";
-      $n("#feedlyFrame").addEventListener("scroll", fnViewStars);
-      _log("fnOnScroll", "列表滚动监听");
-    }
-  }
-
-  // 收藏数 View
-  function fnViewStars() {
-    const objTime = {
-      time: 0,
-      rem: 0,
-    };
-    if (fnCheckControl(gob.diffStars, objTime) === "lock") {
-      $n(".list-entries").style.backgroundColor = "#ddd";
-    }
-    const strText = `Read later（${gob.curStars} 丨 -${gob.diffStars.decr} 丨 +${gob.diffStars.incr}）（${objTime.time} - ${objTime.rem}）`;
-    $n("h1 #header-title").innerHTML = strText;
-    if ($n("header.header h2")) {
-      $n("header.header h2").innerHTML = strText;
-    }
-    $n("#header-title").innerHTML = strText;
-  }
-
-  const curTime = Math.floor(curDate.getTime() / 1000);
-  const curHours = Math.floor(curTime / 3600);
-  // const cur4Hours = Math.floor(curTime / (60 * 60 * 4));
-  const cur4Minutes = Math.floor(curTime / 240);
-
-  const fnCheckControl = (diffStars, objSec = {}) => {
-    const iTime = curHours;
-    objSec.time = iTime;
-    objSec.rem = iTime % 4;
-    if (diffStars.decr < 17) {
-      return "default";
-    }
-    if (iTime % 4 === 0 || diffStars.incr > 13) {
-      return "reset";
-    } else {
-      return "lock";
-    }
-  };
-
-  // 星标变动控制
-  function fnLaterControl() {
-    if (gob.loaded) {
-      return;
-    }
-    gob.load();
-
-    // 解锁重置判断
-    if (gob.bolReset) {
-      gob.diffStars.decr = 0;
-      gob.diffStars.incr = 0;
-    }
-
-    // 星标变化计数
-    const diff = gob.curStars - gob.lstStars;
-
-    // 大于上一次记录
-    if (diff > 0) {
-      // 新增星标计数
-      gob.diffStars.incr += Math.abs(diff);
-    } else {
-      // 已读星标计数
-      gob.diffStars.decr += Math.abs(diff);
-    }
-
-    // 记录变量原始值
-    const strReset = gob.bolReset.toString();
-
-    // _log("fnLaterControl", strReset);
-
-    gob.bolReset = ((diffStars) => {
-      if (fnCheckControl(diffStars) === "reset") {
-        return true;
-      }
-      return false;
-    })(gob.diffStars);
-
-    gob.lstStars = gob.curStars;
-
-    // 更新 localStorage 存储
-    if (diff !== 0 || strReset !== gob.bolReset.toString()) {
-      gob.save();
-    }
-  }
-
-  // 按规则给星标条目着色
-  function fnColorStars() {
-    // _log("fnColorStars", curTime, cur4Minutes);
-
-    const $stars = $na("div.content a");
-    let pickCount = 0;
-    [].forEach.call($stars, function ($e, i) {
-      // _log("fnColorStars", $e, i);
-      // _log("fnColorStars", "==============================");
-
-      const href = $e.href;
-      const hash = parseInt(href.replace(/\D/g, ""));
-
-      // _log("fnColorStars", href, hash);
-
-      const intNum = parseInt(hash + cur4Minutes + i);
-      // const intNum = parseInt(i + cur4Minutes);
-
-      // _log("fnColorStars", intNum, intNum % 4);
-
-      if (intNum % 4 === 0) {
-        pickCount++;
-        $e.parentNode.parentNode.style.backgroundColor = "#ddd";
-      } else {
-        $e.parentNode.parentNode.style.backgroundColor = "transparent";
-        if (fnCheckControl(gob.diffStars) === "lock" || pickCount > 13) {
-          // console.log($e.parentNode.parentNode.classList);
-          $e.parentNode.parentNode.style.backgroundColor = "#666";
-        }
-      }
-    });
-  }
+  }, false);
 
   // 自动标记已读
   (() => {
@@ -393,7 +409,7 @@
         return objRlt;
       }
       return objDef;
-    }
+    };
     // 事件处理函数
     const fnEventHandler = (event) => {
       // 限制鼠标在元素右侧移入才会触发
@@ -418,5 +434,4 @@
     $n("#box").addEventListener("mouseover", fnEventHandler, false);
   })();
 
-  return;
 })();
