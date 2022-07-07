cf87f8144916fd5dd315076395ce93e17937a61b
wdssmq <wdssmq@qq.com>
2022-07-07 16:35:16 +0800

    build. 2022-07-07；

--- bilibili/later.user.js
+++ bilibili/later.user.js
@@ -18,6 +18,7 @@
 // @include      https://t.bilibili.com/*
 // @include      https://manga.bilibili.com/account-center*
 // @include      https://account.bilibili.com/account/big/myPackage
+// @match        https://space.bilibili.com/44744006/fans/follow*
 // @icon         https://www.bilibili.com/favicon.ico
 // @run-at       document-end
 // @grant        GM_setClipboard
@@ -41,23 +42,15 @@
   function $n(e) {
     return document.querySelector(e);
   }
-  // cookie 封装
-  const ckeObj = {
-    setItem: function (key, value) {
-      const Days = 137;
-      const exp = new Date();
-      exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
-      document.cookie = key + "=" + encodeURIComponent(value) + ";path=/;domain=.bilibili.com;expires=" + exp.toGMTString();
-    },
-    getItem: function (key, def = "") {
-      const reg = new RegExp("(^| )" + key + "=([^;]*)(;|$)");
-      const arr = document.cookie.match(reg);
-      if (arr) {
-        return decodeURIComponent(arr[2]);
-      }
-      return def;
-    }
-  };
+  function $na(e) {
+    return document.querySelectorAll(e);
+  }
+  // ---------------------------------------------------
+  // 添加内容到指定元素后面
+  function fnAfter($ne, e) {
+    const $e = typeof e === 'string' ? $n(e) : e;
+    $e.parentNode.insertBefore($ne, $e.nextSibling);
+  }
   // 元素变化监听
   const fnElChange = (el, fn = () => { }) => {
     const observer = new MutationObserver((mutationRecord, mutationObserver) => {
@@ -76,13 +69,30 @@
     });
   };
   // 点击指定元素复制内容
-  function fnCopy(eTrig, content,fnCB = () => { }) {
+  function fnCopy(eTrig, content, fnCB = () => { }) {
     $n(eTrig).addEventListener("click", function (e) {
       GM_setClipboard(content);
       fnCB(e);
       this.style.color = "gray";
     });
   }
+  // cookie 封装
+  const ckeObj = {
+    setItem: function (key, value) {
+      const Days = 137;
+      const exp = new Date();
+      exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
+      document.cookie = key + "=" + encodeURIComponent(value) + ";path=/;domain=.bilibili.com;expires=" + exp.toGMTString();
+    },
+    getItem: function (key, def = "") {
+      const reg = new RegExp("(^| )" + key + "=([^;]*)(;|$)");
+      const arr = document.cookie.match(reg);
+      if (arr) {
+        return decodeURIComponent(arr[2]);
+      }
+      return def;
+    }
+  };
 
   // 日期转字符串
   const getDateStr = (date) => {
@@ -274,6 +284,74 @@
       });
       return false;
     }
+  })();
+
+  // 关注列表增强
+  (() => {
+    const gob = {
+      done: false,
+      _rssUrl: (uid) => {
+        let RSSHub = localStorage.RSSHub ? localStorage.RSSHub : "https://rsshub.app/bilibili/user/video/:uid/";
+        localStorage.RSSHub = RSSHub;
+        return RSSHub.replace(":uid", uid);
+      }
+    };
+    // 获取关注列表
+    const fnGetFollowList = () => {
+      let $list = $na("li.list-item");
+      return $list;
+    };
+
+    // 针对每个关注列表项
+    const fnUpView = ($up) => {
+      const url = $up.querySelector("a.cover").href;
+      const name = $up.querySelector("span.fans-name").innerText;
+
+      const { uid, uname } = ((url, name) => {
+        const uid = url.match(/\/(\d+)\/$/)[1];
+        let uname = name.replace(/\b/g, "_").replace(/_+/g, "_");
+        uname = uname.replace(/^_|_$/g, "");
+        // _log(`${uid} ${uname}`);
+        return { uid, uname };
+      })(url, name);
+
+      const $desc = $up.querySelector("p.auth-description") || $up.querySelector("p.desc");
+      if ($desc && $desc.dataset.fnUpView === "done") {
+        return;
+      }
+      $desc.dataset.fnUpView = "done";
+      // _log($desc);
+
+      // 创建新 p 元素
+      const $p = document.createElement("p");
+      const RSSUrl = gob._rssUrl(uid);
+      $p.className = "desc";
+      $p.innerHTML = `
+    <hr>
+    <a href="${RSSUrl}" target="_blank">「RSS」</a> |
+    ${uid} ${uname}
+    `;
+
+      fnAfter($p, $desc);
+    };
+
+    // 页面元素更新监听
+    const fnCheckByDOM = () => {
+      const $ul = $n("ul.relation-list");
+      const $list = fnGetFollowList();
+      if ($list.length > 0 && !$ul.dataset.fnCheckByDOM !== "done") {
+        $ul.dataset.fnCheckByDOM = "done";
+        _log("$list.length = ", $list.length);
+        // nodeList to array
+        const $listArr = Array.from($list);
+        $listArr.forEach(($li) => {
+          fnUpView($li);
+        });
+      }
+      fnElChange($n("body"), fnCheckByDOM);
+    };
+    fnCheckByDOM();
+
   })();
 
 })();
