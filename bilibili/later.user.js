28600d84823ab2b1b67baab1ae576a899120ab53
wdssmq <wdssmq@qq.com>
2022-07-18 13:59:34 +0800

    build. 脚本构建；

--- bilibili/later.user.js
+++ bilibili/later.user.js
@@ -35,9 +35,17 @@
   // 初始常量或函数
   const curUrl = window.location.href;
   const curDate = new Date();
+  const _getDateStr = (date = curDate) => {
+    const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+    return date.toLocaleDateString("zh-CN", options).replace(/\//g, "-");
+  };
+
   // ---------------------------------------------------
+
   const _log = (...args) => console.log(`[${gm_name}]\n`, ...args);
+
   // ---------------------------------------------------
+
   // const $ = window.$ || unsafeWindow.$;
   function $n(e) {
     return document.querySelector(e);
@@ -45,18 +53,22 @@
   function $na(e) {
     return document.querySelectorAll(e);
   }
+
   // ---------------------------------------------------
+
   // 添加内容到指定元素后面
   function fnAfter($ne, e) {
     const $e = typeof e === 'string' ? $n(e) : e;
     $e.parentNode.insertBefore($ne, $e.nextSibling);
   }
+
+  // ---------------------------------------------------
+
   // 元素变化监听
   const fnElChange = (el, fn = () => { }) => {
     const observer = new MutationObserver((mutationRecord, mutationObserver) => {
-      // _log('body attributes changed!!!'); // body attributes changed!!!
-      // _log('mutationRecord = ', mutationRecord); // [MutationRecord]
-      // _log('mutationObserver === observer', mutationObserver === observer); // true
+      // _log('mutationRecord = ', mutationRecord);
+      // _log('mutationObserver === observer', mutationObserver === observer);
       fn(mutationRecord, mutationObserver);
       mutationObserver.disconnect(); // 取消监听，正常应该在回调函数中根据条件决定是否取消
     });
@@ -68,6 +80,7 @@
       subtree: true,
     });
   };
+
   // 点击指定元素复制内容
   function fnCopy(eTrig, content, fnCB = () => { }) {
     $n(eTrig).addEventListener("click", function (e) {
@@ -205,17 +218,13 @@
 
   // 构造 Bash Shell 脚本
   function fnMKShell(arrList) {
-    const today = new Date(); // 获得当前日期
-    const year = today.getFullYear(); // 获得年份
-    const month = today.getMonth() + 1; // 此方法获得的月份是从0---11，所以要加1才是当前月份
-    const day = today.getDate(); // 获得当前日期
-    const arrDate = [year, month, day];
+    const curDateStr = _getDateStr();
     let strRlt =
       'if [ ! -d "bilibili-foldername" ]; then\n' +
       "mkdir bilibili-foldername\n" +
       "fi\n" +
       "cd bilibili-foldername\n\n";
-    strRlt = strRlt.replace(/foldername/g, arrDate.join("-"));
+    strRlt = strRlt.replace(/foldername/g, curDateStr);
     /**
      * e {title:"",href:""}
      */
