8fcb3339b88f646ecdfe37d1103b7f6a8ae13b34
wdssmq <wdssmq@qq.com>
2022-09-13 21:34:36 +0800

    chore. rollup.js 配置

--- xiuno/rollup.config.js
+++ xiuno/rollup.config.js
@@ -1,13 +1,77 @@
-import { gm_name, gm_banner } from './src/__info.js';
-export default [
-  {
-    input: 'src/main.js',
-    output: {
-      name: gm_name,
-      file: `${gm_name}.user.js`,
-      format: 'iife',
-      banner: gm_banner
-    },
-    plugins: []
+import { gm_name, gm_banner } from "./src/__info.js";
+import replace from "@rollup/plugin-replace";
+
+// for test
+import monkey from "rollup-plugin-monkey";
+
+const gobConfig = {
+  gm_file: `${gm_name}.user.js`,
+  gm_banner: gm_banner.trim() + "\n",
+  listen: {
+    host: "localhost",
+    port: "3000"
   },
-];
+  url: null
+};
+
+gobConfig.url = `http://${gobConfig.listen.host}:${gobConfig.listen.port}`;
+
+const prodConfig = {
+  input: "src/main.js",
+  output: {
+    file: gobConfig.gm_file,
+    format: "iife",
+    banner: gobConfig.gm_banner
+  },
+  plugins: []
+};
+
+const devConfig = {
+  input: "src/main.js",
+  output: {
+    dir: "dev",
+    format: "iife",
+    // banner: gobConfig.gm_banner
+    banner: "/* eslint-disable */\n"
+  },
+  plugins: [
+    monkey({
+      listen: gobConfig.listen,
+      onListen(web) {
+        web.server.log.info({
+          "msg": "{{header}} install script for dev {{url}}",
+          "url": `${gobConfig.url}/dev/${gobConfig.gm_file}`
+        });
+        web.server.log.info({
+          "msg": "{{header}} install script for prod {{url}}",
+          "url": `${gobConfig.url}/${gobConfig.gm_file}`
+        });
+      }
+    }),
+  ]
+};
+
+const loaderConfig = {
+  input: "src/__dev.js",
+  output: {
+    file: `dev/${gobConfig.gm_file}`,
+    format: "iife",
+    banner: gobConfig.gm_banner.replace(/(\/\/ @name\s+)/, "$1「dev」")
+  },
+  plugins: [
+    replace({
+      preventAssignment: true,
+      "placeholder.livereload.js": `${gobConfig.url}/livereload.js?snipver=1`,
+      "placeholder.user.js": `${gobConfig.url}/dev/main.js`,
+    }),
+  ]
+};
+
+const rollupConfig = [prodConfig];
+
+if (process.env.NODE_ENV === "dev") {
+  rollupConfig.push(loaderConfig);
+  rollupConfig.push(devConfig);
+}
+
+export default rollupConfig;
