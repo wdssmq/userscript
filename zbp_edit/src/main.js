ed624203ee13ba223f6b9e2d8456c156127652d8
wdssmq <wdssmq@qq.com>
2022-10-03 10:48:57 +0800

    style. eslint

--- zbp_edit/src/main.js
+++ zbp_edit/src/main.js
@@ -8,7 +8,7 @@ $(function () {
       const type = $(this).data("type");
       const act = type ? "PageEdt" : "ArticleEdt";
       $(this).html(
-        `[<a title="编辑" rel="external" href="${window.bloghost}zb_system/cmd.php?act=${act}&id=${id}">编辑</a>]`
+        `[<a title="编辑" rel="external" href="${window.bloghost}zb_system/cmd.php?act=${act}&id=${id}">编辑</a>]`,
       );
     })
     .removeClass("is-hidden hidden");
@@ -18,25 +18,25 @@ $(function () {
     const $this = $(this);
     const authName = $this.data("name");
     $this.append(
-      ` <a class="cmt-edit" title="查找编辑" rel="external" href="${window.bloghost}zb_users/plugin/cmt2rss/main.php?act=update&read_getWord=${authName}" target="_blank">查找编辑</a>`
+      ` <a class="cmt-edit" title="查找编辑" rel="external" href="${window.bloghost}zb_users/plugin/cmt2rss/main.php?act=update&read_getWord=${authName}" target="_blank">查找编辑</a>`,
     );
   });
   $(".cmt-edit").css({ color: "#175199" });
 
   // 设置文章为回收
   $("#edtTitle").after(
-    '<a class="js-empty" href="javascript:;" title="设置为回收"> [设置为回收]</a>'
+    "<a class=\"js-empty\" href=\"javascript:;\" title=\"设置为回收\"> [设置为回收]</a>",
   );
   let editor_api = window.editor_api;
   $(".js-empty").click(function () {
     $("#edtTitle").val("回收");
     $("#edtTag").val("回收");
-    $('#edtDateTime').datetimepicker('setDate', (new Date()));
+    $("#edtDateTime").datetimepicker("setDate", (new Date()));
     let strMore = "";
     if (typeof window.EDITORMD == "object") {
       strMore = "\n\n<!--more-->";
     } else {
-      strMore = '<hr class="more" />';
+      strMore = "<hr class=\"more\" />";
     }
     let oBody = "回收" + strMore;
     editor_api.editor.content.put(oBody);
