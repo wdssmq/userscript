dbb79d463650246cfb8e5995adc71e4c62b3231c
wdssmq <wdssmq@qq.com>
2022-10-03 10:48:17 +0800

    base. eslint 规范和 ci 命令

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,12 +9,18 @@ module.exports = {
     "ecmaVersion": "latest",
     "sourceType": "module",
   },
+  "globals": {
+    "GM_addStyle": true,
+    "GM_getValue": true,
+    "GM_setValue": true,
+    "unsafeWindow": true,
+  },
   "rules": {
     "generator-star-spacing": 0, // generator 函数中 * 号前后的空格
     "no-unused-vars": [0, { "args": "none" }], // 变量声明后未使用
     // ------------------------------
     "comma-dangle": [1, "always-multiline"], // 对象或数组的拖尾逗号
-    "arrow-parens":  [1, "as-needed", { "requireForBlockBody": true }], // 箭头函数参数括号
+    "arrow-parens": [1, "as-needed", { "requireForBlockBody": true }], // 箭头函数参数括号
     "space-before-function-paren": [1, { "anonymous": "always", "named": "never" }], // 函数圆括号之前的空格
     // ------------------------------
     "semi": [2, "always", { "omitLastInOneLineBlock": true }], // 语句强制分号结尾
