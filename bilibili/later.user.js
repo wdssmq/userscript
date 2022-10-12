61ca2dbb10838b6045fb8cffda560035a9e2b26a
wdssmq <wdssmq@qq.com>
2022-10-12 17:35:13 +0800

    build. 构建发布 - 哔哩哔哩

--- bilibili/later.user.js
+++ bilibili/later.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         「bilibili」大会员 B 币领取提醒
 // @namespace    wdssmq.com
-// @version      0.3
+// @version      0.4
 // @author       沉冰浮水
 // @description  B 币领取提醒、稍后再看列表导出为 *.url 等
 // @url          https://greasyfork.org/scripts/398415
@@ -19,6 +19,7 @@
 // @include      https://account.bilibili.com/account/big/myPackage
 // @match        https://space.bilibili.com/44744006/fans/follow*
 // @icon         https://www.bilibili.com/favicon.ico
+// @noframes
 // @run-at       document-end
 // @grant        GM_setClipboard
 // @grant        GM_notification
@@ -38,6 +39,10 @@
   // 初始常量或函数
   const curUrl = window.location.href;
   const curDate = new Date();
+
+  // ---------------------------------------------------
+
+  const _curUrl = () => { return window.location.href };
   const _getDateStr = (date = curDate) => {
     const options = { year: "numeric", month: "2-digit", day: "2-digit" };
     return date.toLocaleDateString("zh-CN", options).replace(/\//g, "-");
@@ -428,5 +433,71 @@
   };
   // 当页面内容产生变化时，触发函数 fnMain
   fnElChange($body, fnMain, false);
+
+  // 时间轴书签
+
+  const gob = {
+    lstTime: 0,
+    curTime: 0,
+    title: "",
+  };
+
+  const fnUpTitle = (time) => {
+    if (gob.title === "") {
+      gob.title = $n("h1.video-title").innerHTML;
+    }
+    $n("title").innerHTML = `${gob.title}_${time}_bilibili`;
+    // debug
+    _warn(`title: ${gob.title}_${time}_bilibili`);
+  };
+
+  const fnUpUrl = (time) => {
+    let url = _curUrl();
+    let urlNew = url;
+    // 清理参数
+    url = url.split("?")[0];
+    // 拼接参数
+    urlNew = `${url}?t=${time}`;
+    window.history.pushState(null, null, urlNew);
+    // debug
+    _warn(`url: ${urlNew}`);
+  };
+
+  const fnGetTime = () => {
+    const $timLabel = $n(".bpx-player-ctrl-time-label");
+    const $curTime = $n(".bpx-player-ctrl-time-current");
+    if ($timLabel && $curTime) {
+      const str = $curTime.innerHTML;
+      const arr = str.split(":");
+      if (arr.length === 3) {
+        return parseInt(arr[0]) * 3600 + parseInt(arr[1]) * 60 + parseInt(arr[2]);
+      } else {
+        return parseInt(arr[0]) * 60 + parseInt(arr[1]);
+      }
+    }
+  };
+
+  document.addEventListener(
+    "mouseover",
+    function (e) {
+      const $target = e.target;
+      // bpx-player-container bpx-state-no-cursor
+
+      // const $container = $n(".bpx-player-container");
+      // if ($container && !$container.classList.contains("bpx-state-no-cursor")) {
+      //   _warn("进度条", e.target);
+      //   return;
+      // }
+
+      if ($target.classList.contains("bpx-player-control-wrap")) {
+        gob.curTime = fnGetTime();
+        if (gob.curTime > 0 && gob.curTime - gob.lstTime > 137) {
+          fnUpTitle(gob.curTime);
+          fnUpUrl(gob.curTime);
+        }
+      }
+    },
+    false,
+  );
 
 })();
