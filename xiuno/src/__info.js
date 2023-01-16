0fb4c8ea67740d1a5b4a29c087ec8ef47b1de95f
wdssmq <wdssmq@qq.com>
2023-01-16 20:11:27 +0800

    up. gm_info

--- xiuno/src/__info.js
+++ xiuno/src/__info.js
@@ -1,19 +1,23 @@
 const gm_banner = `
 // ==UserScript==
 // @name         「xiuno」管理工具（QQ 群：189574683）
-// @namespace    沉冰浮水
+// @namespace    https://www.wdssmq.com/
 // @version      placeholder.pkg.version
-// @description  对不合规的内容加密处理
 // @author       沉冰浮水
+// @description  对不合规的内容加密处理
+// @license      MIT
 // @link         https://greasyfork.org/zh-CN/scripts/419517
-// @null     ----------------------------
+// @null         ----------------------------
 // @contributionURL    https://github.com/wdssmq#%E4%BA%8C%E7%BB%B4%E7%A0%81
 // @contributionAmount 5.93
-// @null     ----------------------------
-// @link     https://github.com/wdssmq/userscript
-// @link     https://afdian.net/@wdssmq
-// @link     https://greasyfork.org/zh-CN/users/6865-wdssmq
-// @null     ----------------------------
+// @null         ----------------------------
+// @link         https://github.com/wdssmq/userscript
+// @link         https://afdian.net/@wdssmq
+// @link         https://greasyfork.org/zh-CN/users/6865-wdssmq
+// @null         ----------------------------
+// @noframes
+// @run-at       document-end
+
 // @match        https://bbs.zblogcn.com/*
 // @require      https://cdn.bootcdn.net/ajax/libs/lz-string/1.4.4/lz-string.min.js
 // @require      https://cdn.bootcdn.net/ajax/libs/js-yaml/4.1.0/js-yaml.min.js
@@ -21,7 +25,9 @@ const gm_banner = `
 // @grant        GM_setValue
 // @grant        GM_getValue
 // ==/UserScript==
-/* jshint esversion:6 */
+
+/* eslint-disable */
+/* jshint esversion: 6 */
 `;
 
 const gm_name = "xiuno";
