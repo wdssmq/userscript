976c2300d033610222314e5bd10c4fc17b0fbdd3
wdssmq <wdssmq@qq.com>
2023-03-08 14:23:23 +0800

    build. 哔哩哔哩

--- bilibili/later.user.js
+++ bilibili/later.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         「bilibili」大会员 B 币领取提醒
+// @name         「bilibili」水水自用 | B 币领取提醒、播放进度更新至网址等；
 // @namespace    wdssmq.com
-// @version      1.0.0
+// @version      1.0.1
 // @author       沉冰浮水
 // @description  B 币领取提醒、稍后再看列表导出为 *.url 等
 // @url          https://greasyfork.org/scripts/398415
@@ -442,27 +442,54 @@
   const gob = {
     lstTime: 0,
     curTime: 0,
+    curPart: 0,
     isRunning: false,
     title: "",
     step: 73,
   };
 
-  const fnUpTitle = (time) => {
+  const fnUpTitle = (part, time) => {
     if (gob.title === "") {
       gob.title = $n("h1.video-title").innerHTML;
     }
-    $n("title").innerHTML = `${gob.title}_${time}_bilibili`;
+    const strAdd = ((obj) => {
+      let str = "";
+      for (const key in obj) {
+        if (Object.hasOwnProperty.call(obj, key)) {
+          const val = obj[key];
+          if (!val) {
+            continue;
+          }
+          const s = key === "part" ? "P" : "播放至 ";
+          str += `_${s}${val}`;
+        }
+      }
+      return str;
+    })({ part, time });
+    $n("title").innerHTML = `${gob.title}${strAdd}_bilibili`;
     // debug
-    _warn(`title: ${gob.title}_${time}_bilibili`);
+    _warn(`title: ${$n("title").innerHTML}`);
   };
 
-  const fnUpUrl = (time) => {
+  const fnUpUrl = (p, time) => {
     let url = _curUrl();
     let urlNew = url;
     // 清理参数
     url = url.split("?")[0];
+    // _warn("fnUpUrl", { p, time });
+    // 新参数构造
+    const params = new URLSearchParams();
+    for (const key in { p, time }) {
+      if (Object.hasOwnProperty.call({ p, time }, key)) {
+        const item = { p, time }[key];
+        if (item) {
+          params.append(key, item);
+        }
+      }
+    }
     // 拼接参数
-    urlNew = `${url}?t=${time}`;
+    urlNew = `${url}?${params.toString()}`;
+    // 更新至地址栏
     window.history.pushState(null, null, urlNew);
     // debug
     _warn(`url: ${urlNew}`);
@@ -481,6 +508,21 @@
       }
     }
     return 0;
+  };
+
+  const fnGetPart = () => {
+    const $list = $n("ul.list-box");
+    const $cur = $list?.querySelector("li.on");
+    const $curHref = $cur?.querySelector("a");
+    if ($cur && $curHref) {
+      // _warn($cur, $curHref);
+      const str = $curHref.href;
+      const arr = str.split("?p=");
+      if (arr.length === 2) {
+        return parseInt(arr[1]);
+      }
+    }
+    return null;
   };
 
   const fnDelay = (time, fnc = () => { }) => {
@@ -504,18 +546,23 @@
         let bolFlag = false;
         if ($container && !$container.classList.contains("bpx-state-no-cursor")) {
           gob.curTime = fnGetTime();
+          gob.curPart = fnGetPart();
           if (gob.curTime > 0 && gob.curTime - gob.lstTime > gob.step) {
             bolFlag = true;
           }
           if (gob.curTime < gob.lstTime) {
             bolFlag = true;
           }
+          if (gob.lstTime <= parseInt(gob.step / 4)) {
+            bolFlag = true;
+          }
           if (bolFlag) {
-            fnUpTitle(gob.curTime);
-            fnUpUrl(gob.curTime);
+            fnUpTitle(gob.curPart, gob.curTime);
+            fnUpUrl(gob.curPart, gob.curTime);
             gob.lstTime = gob.curTime;
           }
-          _warn("进度条触发", e.target);
+          // _warn("进度条触发", e.target);
+          // _warn("进度条触发", gob);
           return;
         }
       });
