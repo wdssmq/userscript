82b3533c00bfcc01a73eef6a4380f4ea26621fdd
wdssmq <wdssmq@qq.com>
2023-03-20 17:02:39 +0800

    chore. 构建工具及配置

--- Mikan_Proj/src/__info.js
+++ Mikan_Proj/src/__info.js
@@ -2,7 +2,7 @@ const gm_banner = `
 // ==UserScript==
 // @name         「蜜柑计划」列表过滤（简繁/画质）
 // @namespace    https://www.wdssmq.com/
-// @version      0.1
+// @version      placeholder.pkg.version
 // @author       沉冰浮水
 // @description  过滤蜜柑计划列表，按照简繁/画质过滤
 // @license      MIT
@@ -15,6 +15,7 @@ const gm_banner = `
 // @link         https://greasyfork.org/zh-CN/users/6865-wdssmq
 // @null         ----------------------------
 // @noframes
+// @run-at       document-end
 // @match        https://mikanani.me/Home/Bangumi/*
 // @match        https://feedly.com/i/subscription/feed*
 // @grant        GM_getValue
@@ -24,9 +25,17 @@ const gm_banner = `
 // @grant        GM_setClipboard
 // ==/UserScript==
 
+/* eslint-disable */
 /* jshint esversion: 6 */
 `;
 
 const gm_name = "Mikan_Proj";
 
-export { gm_banner, gm_name };
+const gm_require = [
+  {
+    "url": "https://cdn.bootcdn.net/ajax/libs/jquery/3.6.3/jquery.min.js",
+    "func": "$",
+  },
+];
+
+export { gm_banner, gm_name, gm_require };
