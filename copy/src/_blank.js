3535c8394a166b3ca0a2bd088222c7cea966c812
wdssmq <wdssmq@qq.com>
2023-04-05 17:02:14 +0800

    up. 指定页面链接设置新窗口打开

---
+++ copy/src/_blank.js
@@ -0,0 +1,26 @@
+import { _log, $na, fnFindDom } from "./_base";
+
+// 指定元素中的链接增加 target="_blank"
+const config = [
+    [".markdown_body", ".reply_content"],
+    ["#additional-info"],
+];
+
+const fnSetBlank = ($a) => {
+    $a.setAttribute("target", "_blank");
+};
+
+config.forEach((e) => {
+    const selector = e.join(",");
+    const $$container = $na(selector);
+    // // print $$container
+    // _log($$container);
+    // 遍历 $$container
+    [].forEach.call($$container, ($el) => {
+        const $$a = fnFindDom($el, "a");
+        // _log($$a);
+        if ($$a.length > 0) {
+            [].map.call($$a, fnSetBlank);
+        }
+    });
+});
--- copy/src/main.js
+++ copy/src/main.js
@@ -1,4 +1,7 @@
 // _def.js
 import _def from "./_def.js";
 
+// _blank.js
+import _blank from "./_blank.js";
+
 // 具体功能在单独的文件中实现，这里只是导入
