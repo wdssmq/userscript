58ab58aa47a97dcc6632b4f57542a7ba2bfa9c6e
wdssmq <wdssmq@qq.com>
2023-08-18 17:12:36 +0800

    build. GesF-Note

--- zbp-TimeLine/zbp-TimeLine.user.js
+++ zbp-TimeLine/zbp-TimeLine.user.js
@@ -16,6 +16,7 @@
 // @noframes
 // @run-at       document-end
 // @match        https://www.wdssmq.com/*
+// @match        https://github.com/wdssmq/*
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM_setClipboard
@@ -28,6 +29,9 @@
   'use strict';
 
   const gm_name = "zbp-TimeLine";
+
+  // 初始常量或函数
+  const curUrl = window.location.href;
 
   // -------------------------------------
 
@@ -54,7 +58,7 @@
   }
 
   // note 条目格式定义
-  const noteScheme$1 = {
+  const noteScheme$2 = {
     item: {
       "Title": "node:.post-title a",
       "Desc": "node:.post-intro",
@@ -72,13 +76,11 @@
 
   // 初始化配置
   const config = GM_getValue("config", {});
-  if (JSON.stringify(config) === "{}" || config.noteScheme.ver !== noteScheme$1.ver) {
+  if (JSON.stringify(config) === "{}" || config.noteScheme.ver !== noteScheme$2.ver) {
     GM_setValue("config", {
-      noteScheme: noteScheme$1,
+      noteScheme: noteScheme$2,
     });
   }
-
-  // alert("getNoteByZBP");
 
   // 格式化 JSON
   function formatJSON(obj) {
@@ -111,21 +113,13 @@
   }
 
   // 对象转 yaml
-  function obj2yaml(obj) {
+  function formatYAML(obj) {
     let strYaml = "";
     obj = formatData(obj);
     Object.keys(obj).forEach((key) => {
       strYaml += `${key}: ${obj[key]}\n`;
     });
     return strYaml;
-  }
-
-  function btnToggle($btn, text) {
-    const tmp = $btn.text;
-    $btn.text = text;
-    setTimeout(() => {
-      $btn.text = tmp;
-    }, 3000);
   }
 
   // 添加复制按钮及事件的封装
@@ -141,7 +135,7 @@
     if ("json" === copyType) {
       copyText = formatJSON(note);
     } else if ("yaml" === copyType) {
-      copyText = obj2yaml(note);
+      copyText = formatYAML(note);
     }
     // 复制按钮事件
     $btn.addEventListener("click", () => {
@@ -151,17 +145,28 @@
     });
   }
 
-  const noteScheme = config.noteScheme;
-  const $items = $na(noteScheme.parent);
+  // 按钮文本切换
+  function btnToggle($btn, text) {
+    const tmp = $btn.text;
+    $btn.text = text;
+    setTimeout(() => {
+      $btn.text = tmp;
+    }, 3000);
+  }
+
+  // alert("getNoteByZBP");
+
+  const noteScheme$1 = config.noteScheme;
+  const $items = $na(noteScheme$1.parent);
   Array.from($items).forEach(($item) => {
     // 移除不需要的元素
-    const $remove = fnFindDom($item, noteScheme.remove);
+    const $remove = fnFindDom($item, noteScheme$1.remove);
     if ($remove) {
       $remove.remove();
     }
     const note = {};
-    Object.keys(noteScheme.item).forEach((key) => {
-      const selector = noteScheme.item[key];
+    Object.keys(noteScheme$1.item).forEach((key) => {
+      const selector = noteScheme$1.item[key];
       if (!selector) {
         return;
       }
@@ -189,7 +194,7 @@
       }
     });
     // 添加复制按钮
-    const $btnWrap = fnFindDom($item, noteScheme.btnWrap);
+    const $btnWrap = fnFindDom($item, noteScheme$1.btnWrap);
     // _log("$btnWrap", $btnWrap);
     if ($btnWrap) {
       addCopyBtn($btnWrap, note, "复制 JSON", "json");
@@ -219,5 +224,60 @@
   // }
 
   // const themeName = _themeName();
+
+  const noteScheme = config.noteScheme;
+
+  // 获取 github 仓库基本信息
+  async function git_repoInfo() {
+    const repoInfo = {};
+    repoInfo.path = curUrl.replace("https://github.com/", "");
+    // 使用 api 获取
+    const url = `https://api.github.com/repos/${repoInfo.path}`;
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      repoInfo.desc = data.description;
+      repoInfo.tags = data.topics;
+      repoInfo.data = data;
+      _log("repoInfo", repoInfo);
+    } else {
+      repoInfo.data = null;
+      _log("response", response);
+    }
+    return repoInfo;
+  }
+
+  // update noteScheme
+  function git_noteScheme(repoInfo) {
+    const { path, desc, tags } = repoInfo;
+    noteScheme.item.Desc = desc;
+    noteScheme.item.Source = "[url=https://github.com/wdssmq]wdssmq (沉冰浮水)@github[/url]";
+    noteScheme.item.Tags = ["GitHub"].concat(tags);
+    noteScheme.item.Title = `GitHub - ${path}`;
+    noteScheme.item.Type = "代码";
+    noteScheme.item.Url = curUrl;
+    // _log("noteScheme", noteScheme);
+  }
+
+  // 设置复制按钮
+  function git_btnCopy() {
+    const $base = $n(".BorderGrid-cell>div");
+    // _log("$base", $base);
+    if (!$base) return;
+    addCopyBtn($base, noteScheme.item, "复制 YAML", "yaml");
+    // insertAdjacentHTML 添加一个 span
+    $base.insertAdjacentHTML("beforeend", "<span class=\"is-pulled-right\">&nbsp;&nbsp;</span>");
+    addCopyBtn($base, noteScheme.item, "复制 JSON", "json");
+  }
+
+  (async () => {
+    if (!curUrl.includes("github.com/wdssmq")) return;
+    const repoInfo = await git_repoInfo();
+    git_noteScheme(repoInfo);
+    git_btnCopy();
+  })();
+
+
+  // Object.keys(noteScheme.item).forEach((key) => {
 
 })();
