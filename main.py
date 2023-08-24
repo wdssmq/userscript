7d24ad1916382166e321704213d65d463e1d0384
wdssmq <wdssmq@qq.com>
2023-08-24 20:06:33 +0800

    py. 重构准备

---
+++ main.py
@@ -0,0 +1,16 @@
+""" 项目入口文件 """
+import os
+
+# 全局变量
+# README.md
+_readme_file = os.path.join(os.getcwd(), "README.md")
+
+
+# 主函数定义
+def fnMain():
+    """主函数"""
+    print("Hello World!")
+
+
+# 调用主函数
+fnMain()
