7d475d54908ec4c947260b0e947aac4301c515be
GitHub Action <action@github.com>
2023-08-26 18:30:58 +0800

    Auto Commit

---
+++ docs/chunks/copy.36f3258c.mjs
@@ -0,0 +1,9 @@
+async function getMod() {
+						return import('./copy.f2c5be62.mjs');
+					}
+					const collectedLinks = "@@ASTRO-LINKS@@";
+					const collectedStyles = "@@ASTRO-STYLES@@";
+					const collectedScripts = "@@ASTRO-SCRIPTS@@";
+					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
+
+export { defaultMod as default };
