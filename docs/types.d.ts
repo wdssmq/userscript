1d791018e980901c9d48d81ed3ba874dd45eae37
GitHub Action <action@github.com>
2023-08-26 19:31:31 +0800

    Auto Commit

--- docs/types.d.ts
+++ docs/types.d.ts
@@ -195,6 +195,13 @@ declare module 'astro:content' {
   collection: "gm_md";
   data: InferEntrySchema<"gm_md">
 } & { render(): Render[".md"] };
+"qBit.md": {
+	id: "qBit.md";
+  slug: "qbit";
+  body: string;
+  collection: "gm_md";
+  data: InferEntrySchema<"gm_md">
+} & { render(): Render[".md"] };
 };
 
 	};
