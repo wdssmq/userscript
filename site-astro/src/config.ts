4b578f6160a1d01407fb6d0ae0ae8e1baabda7f2
wdssmq <wdssmq@qq.com>
2023-08-28 16:33:14 +0800

    astro. 站点标题调整

--- site-astro/src/config.ts
+++ site-astro/src/config.ts
@@ -1,5 +1,9 @@
 
 export const config = {
+  site:{
+    title: "wdssmq/userscript",
+    description: "各种「GM_脚本」",
+  },
   func: {
     sortPosts: (a: any, b: any) => b.data.updateDate - a.data.updateDate,
   }
