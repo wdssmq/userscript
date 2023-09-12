5305902e183cf24a18b9f68acee82a5733fafc57
wdssmq <wdssmq@qq.com>
2023-09-12 11:29:07 +0800

    mv. lib-menu

---
+++ packages-lib/lib-menu/rollup.config.mjs
@@ -0,0 +1,26 @@
+// import pkg from "./package.json" assert {type: 'json'};
+import { readFileSync } from "fs";
+const pkg = JSON.parse(readFileSync("./package.json"));
+import postcss from "rollup-plugin-postcss";
+
+const defConfig = {
+    input: "src/lib-menu.js",
+    plugins: [
+        postcss(),
+    ],
+    output:
+    {
+        file: pkg.main,
+        format: "umd",
+        name: pkg.moduleName,
+        banner: "/* eslint-disable */\n",
+        // sourcemap: true,
+    },
+
+};
+
+if (process.env.NODE_ENV === "prod") {
+    defConfig.output.file = `../../dist-lib/${pkg.name}.js`;
+}
+
+export default defConfig;
