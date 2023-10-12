d387ae3fc24ee36f459834d2d1231d2ea07402e6
wdssmq <wdssmq@qq.com>
2023-10-12 11:39:53 +0800

    up. 分 P 链接获取 - 哔哩哔哩

--- packages/bilibili/src/_later2url.js
+++ packages/bilibili/src/_later2url.js
@@ -4,7 +4,7 @@ import {
   fnCopy,
   fnFindDom,
   $n,
-  $na
+  $na,
 } from "./_base";
 
 _log("_later2url.js", "开始");
@@ -78,45 +78,81 @@ function fnGetAjax(callback = function () { }) {
 
 // 分 p 链接获取
 (() => {
-  const selectorMap = {
-    epListBox: ".eplist_ep_list_wrapper__PzLHa",
-    epListTitle: ".eplist_list_title__JwP3d h4",
-    // epList: ".imageList_wrap__pDHvN",
-    epList: ".imageList_wrap__pDHvN .imageListItem_wrap__HceXs a",
+  // 【个人中字】擅长捉弄的高木同学 特典广播剧1~6 第一季BD特典_哔哩哔哩_bilibili
+  // https://www.bilibili.com/video/BV1XZ4y1R74P/
+  const gob = {
+    // 选择器
+    epBox: "#multi_page",
+    epBoxTitle: ".head-con h3",
+    epList: ".cur-list .list-box li a",
+    // 状态
+    epLoaded: false,
   };
-  const $$epListBox = $na(selectorMap.epListBox);
-  if ($$epListBox.length === 0) {
+  const $$epBox = $na(gob.epBox);
+  _log("$$epBox", $$epBox);
+  if ($$epBox.length === 0) {
     return;
   }
+
+  // 获取 DOM
+  const fnGetDom = ($epBox) => {
+    const $epBoxTitle = $epBox.querySelector(gob.epBoxTitle);
+    const $epList = $epBox.querySelectorAll(gob.epList);
+    return {
+      $epBoxTitle,
+      $epList,
+    };
+  };
+
+  // 检查是否加载完毕
+  const fnCheckLoaded = (callback) => {
+    if (gob.epLoaded) {
+      callback();
+      return;
+    }
+    const { $epBoxTitle, $epList } = fnGetDom($$epBox[0]);
+    if ($epBoxTitle && $epList.length > 0) {
+      gob.epLoaded = true;
+    }
+    setTimeout(() => {
+      fnCheckLoaded(callback);
+    }, 3000);
+  };
+
   const fnGetLinkList = ($epList) => {
     // _log("fnOnCLick", $epList);
     const arrList = Array.prototype.map.call($epList, ($item, index) => {
       const href = $item.href;
-      const title = $item.innerText.replace(/\n.+/g, "");
+      const title = $item.innerText.replace(/\n[^\n]+$/g, "").replace("\n", "_");
       return {
         href,
         title,
+        innerText: $item.innerText,
       };
     });
     _log("arrList", arrList);
     return arrList;
   };
-  
-  const elListDom = [];
-  // 遍历 NodeList，不能直接使用 forEach
-  Array.prototype.forEach.call($$epListBox, ($epListBox, index) => {
-    const $epListTitle = fnFindDom($epListBox, selectorMap.epListTitle);
-    const $epList = fnFindDom($epListBox, selectorMap.epList);
-    const textTitle = $epListTitle.innerText;
-    // 注册点击复制
-    fnCopy($epListTitle, fnMKShell(fnGetLinkList($epList), textTitle));
-    elListDom.push({
-      box: $epListBox,
-      title: $epListTitle,
-      list: $epList,
+
+  const fnMain = () => {
+    // const elListDom = [];
+    // 遍历 NodeList，不能直接使用 forEach
+    Array.prototype.forEach.call($$epBox, ($epBox, index) => {
+      const { $epBoxTitle, $epList } = fnGetDom($epBox);
+      const textTitle = $epBoxTitle.innerText;
+      // 注册点击复制
+      fnCopy($epBoxTitle, fnMKShell(fnGetLinkList($epList), textTitle));
+      // elListDom.push({
+      //   box: $epBox,
+      //   title: $epBoxTitle,
+      //   list: $epList,
+      // });
     });
-  });
-  console.log(elListDom);
+    // console.log(elListDom);
+  };
+
+  // 等待加载完毕
+  fnCheckLoaded(fnMain);
 })();
 
 // 导出稍后再看为 .lnk 文件
