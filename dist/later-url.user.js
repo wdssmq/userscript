bf703571092ad896f3d894e6e5852071e47fdcd2
GitHub Action <action@github.com>
2023-12-26 16:50:03 +0800

    Auto Commit

--- dist/later-url.user.js
+++ dist/later-url.user.js
@@ -15,7 +15,7 @@
 // @null         ----------------------------
 // @noframes
 // @run-at       document-end
-// @match        https://space.bilibili.com/*/video*
+// @match        https://space.bilibili.com/*
 // @grant        GM_xmlhttpRequest
 // @grant        GM_getValue
 // @grant        GM_setValue
