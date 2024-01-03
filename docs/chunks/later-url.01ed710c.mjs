4dc52e5f11efa8b075c62458c29029ae36effc1c
GitHub Action <action@github.com>
2024-01-03 18:01:35 +0800

    Auto Commit

--- docs/chunks/later-url.01ed710c.mjs
+++ docs/chunks/later-url.01ed710c.mjs
@@ -1,5 +1,5 @@
 async function getMod() {
-						return import('./later-url.1b4dfbda.mjs');
+						return import('./later-url.10bbb155.mjs');
 					}
 					const collectedLinks = "@@ASTRO-LINKS@@";
 					const collectedStyles = "@@ASTRO-STYLES@@";
