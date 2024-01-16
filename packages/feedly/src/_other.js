05da129b72dd49f52fcf8d7073df71b751e9d377
wdssmq <wdssmq@qq.com>
2024-01-16 12:06:47 +0800

    up. feedly: 指定格式的条目标题重组

--- packages/feedly/src/_other.js
+++ packages/feedly/src/_other.js
@@ -1,4 +1,9 @@
-import { $n } from "./_base.js";
+import {
+  _log,
+  $n,
+  $na,
+  fnElChange,
+} from "./_base.js";
 
 // 防止误点
 const fnStopSource = (e) => {
@@ -18,4 +23,42 @@ const fnStopSource = (e) => {
 };
 
 $n("#root").addEventListener("click", fnStopSource);
+
+// 条目标题处理
+const fnItemTitle = ($e) => {
+  // _log("fnItemTitle", $e);
+  if ($e.dataset.ptDone) {
+    return;
+  }
+  const strTitle = $e.innerText;
+  const arrMatch = strTitle.match(/(?<cate>\[[^\]]+\])[^[]+-(?<group>[^[]+)(?<title>\[.+\])$/);
+  if (arrMatch) {
+    const strCate = arrMatch.groups.cate.replace(/^\[[^)]+\(([^)]+)\)\]/, "[$1]");
+    const strGroup = arrMatch.groups.group;
+    const strTitle = arrMatch.groups.title;
+    const strNewTitle = `${strTitle} - ${strCate}[${strGroup}]`;
+    $e.innerText = strNewTitle;
+    $e.dataset.ptDone = "1";
+  }
+};
+
+const fnItemTitleWrap = (e) => {
+  const $$list = $na("#feedlyPageFX .entry");
+  if (!$$list.length) {
+    return;
+  }
+  // 遍历并查找 .EntryTitleLink
+  for (let i = 0; i < $$list.length; i++) {
+    const $item = $$list[i];
+    const $title = $item.querySelector(".EntryTitleLink");
+    if ($title) {
+      fnItemTitle($title);
+    }
+  }
+};
+
+fnElChange($n("#root"), () => {
+  fnItemTitleWrap();
+});
+
 
