476ead1c5a03057449c9f1ca31fd1e3f6218e751
wdssmq <wdssmq@qq.com>
2024-06-04 22:59:53 +0800

    add. lib-mizu

---
+++ packages-lib/lib-mizu/rollup.config.mjs
@@ -0,0 +1,69 @@
+// import pkg from "./package.json" assert {type: 'json'};
+import { readFileSync } from "fs";
+const pkg = JSON.parse(readFileSync("./package.json"));
+
+import livereload from "rollup-plugin-livereload";
+import postcss from "rollup-plugin-postcss";
+import serve from "rollup-plugin-serve";
+import copy from "rollup-plugin-copy";
+
+// import html from "rollup-plugin-html-string";
+// import md from "rollup-plugin-md";
+// import resolve from "@rollup/plugin-node-resolve";
+
+import replace from "@rollup/plugin-replace";
+
+// 是否将 CSS 提取到单独文件
+const extractCSS = pkg.extractCSS ? `${pkg.name}.css` : false;
+
+const defConfig = {
+    input: `src/${pkg.name}.js`,
+    output: {
+        file: pkg.main,
+        format: "umd",
+        name: pkg.moduleName,
+        banner: "/* eslint-disable */\n",
+        // sourcemap: true,
+    },
+    plugins: [
+        postcss({
+            extract: extractCSS,
+        }),
+        replace({
+            preventAssignment: true,
+            "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+        }),
+        // resolve(),
+        // md(),
+        // html({
+        //     include: "src/**/*.html",
+        // }),
+    ],
+};
+
+if (process.env.NODE_ENV === "prod") {
+    // 根据 pkg.extractCSS 的值，决定是否为项目创建单独的文件夹
+    const distProd = pkg.extractCSS ? `../../dist-lib/${pkg.name}` : "../../dist-lib";
+    // 将打包好的文件复制到 distProd 目录
+    defConfig.plugins.push(
+        copy({
+            targets: [
+                {
+                    src: pkg.main,
+                    dest: distProd,
+                },
+                {
+                    src: `dist/${pkg.name}.css`,
+                    dest: distProd,
+                },
+            ],
+        }),
+    );
+} else {
+    defConfig.plugins.push(
+        serve(),
+        livereload(),
+    );
+}
+
+export default defConfig;
---
+++ packages-lib/lib-mizu/src/lib-mizu.js
@@ -0,0 +1,6 @@
+import "./style/style.sass";
+
+document.querySelector("#time").textContent = new Date().toLocaleTimeString();
+
+console.log("lib-mizu.js");
+
