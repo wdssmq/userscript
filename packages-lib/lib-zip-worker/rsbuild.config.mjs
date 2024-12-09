d91c910017176ade36a0e9277b1aba9eb6b499ce
wdssmq <wdssmq@qq.com>
2024-12-09 13:38:27 +0800

    chore-rsbuild. 压缩选项

--- packages-lib/lib-zip-worker/rsbuild.config.mjs
+++ packages-lib/lib-zip-worker/rsbuild.config.mjs
@@ -1,3 +1,11 @@
 import { defineConfig } from '@rsbuild/core';
 
-export default defineConfig({});
+export default defineConfig({
+    input: 'src/index.js',
+    output: {
+        minify: {
+            js: false,
+        },
+    },
+    plugins: [],
+});
