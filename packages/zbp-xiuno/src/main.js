fec61176c797d8633a72a04da11210882d4b5772
wdssmq <wdssmq@qq.com>
2025-08-11 10:58:35 +0800

    up-xiuno. 开发者申请废弃

--- packages/zbp-xiuno/src/main.js
+++ packages/zbp-xiuno/src/main.js
@@ -7,8 +7,8 @@ import _lz from "./_lz.js";
 // 楼层地址
 import _pid from "./_pid.js";
 
-// 开发者申请
-import _dev from "./_dev.js";
+// // 开发者申请
+// import _dev from "./_dev.js";
 
 // 编辑器 blockquote
 import _blockquote from "./_blockquote.js";
