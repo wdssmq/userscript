62a2860609e7266256f4c895f22088cf3aaef84f
wdssmq <wdssmq@qq.com>
2025-10-10 22:41:51 +0800

    chore. eslint 配置

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,9 +16,24 @@ export default antfu(
       "packages/**/README.md",
     ],
 
+    languageOptions: {
+      globals: {
+        GM_xmlhttpRequest: "readonly", // 或 "writable"，多数情况用 "readonly"
+      },
+    },
+
     rules: {
       "style/semi": [2, "always", { omitLastInOneLineBlock: true }], // 语句强制分号结尾
+      "no-alert": "off", // 允许使用 alert
+      "no-console": "off", // 允许使用 console
+      "no-unused-vars": "off", // 关闭原生的 no-unused-vars 规则，使用插件的版本
+      "unused-imports/no-unused-vars": ["error", { vars: "all", varsIgnorePattern: "^_" }],
+      "unused-imports/no-unused-imports": ["error", { vars: "all", varsIgnorePattern: "^_" }],
     },
   },
 
 );
+
+// 用于关闭某些规则的注释写法
+// eslint no-unused-vars: "off", @typescript-eslint/no-unused-vars: "off"
+// eslint no-unused-vars: "off", unused-imports/no-unused-imports: "off"
