a63e75b868fe96706b0e421e8f9894737839eb76
wdssmq <wdssmq@qq.com>
2026-04-29 23:29:56 +0800

    chore-lib-menu: 更新 rollup 配置

--- packages-lib/lib-menu/rollup.config.mjs
+++ packages-lib/lib-menu/rollup.config.mjs
@@ -1,32 +1,85 @@
 // import pkg from "./package.json" assert {type: 'json'};
 import { readFileSync } from "node:fs";
+import process from "node:process";
 
+import replace from "@rollup/plugin-replace";
+
+// import typescript from "@rollup/plugin-typescript";
+import copy from "rollup-plugin-copy";
 import livereload from "rollup-plugin-livereload";
 import postcss from "rollup-plugin-postcss";
+
+// import html from "rollup-plugin-html-string";
+// import md from "rollup-plugin-md";
+// import resolve from "@rollup/plugin-node-resolve";
+
 import serve from "rollup-plugin-serve";
 
 const pkg = JSON.parse(readFileSync("./package.json"));
 
+// 是否将 CSS 提取到单独文件
+const extractCSS = pkg.extractCSS ? `${pkg.name}.css` : false;
+
 const defConfig = {
   input: `src/${pkg.name}.js`,
+  output: {
+    file: pkg.main,
+    format: "umd",
+    name: pkg.moduleName,
+    banner: "/* eslint-disable */\n",
+    // sourcemap: true,
+  },
   plugins: [
-    postcss(),
-    serve(),
-    livereload("dist"),
+    // typescript({
+    //   compilerOptions: {
+    //     moduleResolution: "bundler",
+    //     lib: ["esNext", "dom"],
+    //     target: "esNext",
+    //   },
+    // }),
+    postcss({
+      extract: extractCSS,
+    }),
+    replace({
+      "preventAssignment": true,
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+    }),
+    // resolve(),
+    // md(),
+    // html({
+    //     include: "src/**/*.html",
+    // }),
   ],
-  output:
-    {
-      file: pkg.main,
-      format: "umd",
-      name: pkg.moduleName,
-      banner: "/* eslint-disable */\n",
-      // sourcemap: true,
-    },
-
 };
 
 if (process.env.NODE_ENV === "prod") {
-  defConfig.output.file = `../../dist-lib/${pkg.name}.js`;
+  // 根据 pkg.extractCSS 的值，决定是否为项目创建单独的文件夹
+  const distProd = pkg.extractCSS ? `../../dist-lib/${pkg.name}` : "../../dist-lib";
+  const targets = [
+    {
+      src: pkg.main,
+      dest: distProd,
+    },
+  ];
+  if (extractCSS) {
+    targets.push({
+      src: `dist/${pkg.name}.css`,
+      dest: distProd,
+    });
+  }
+
+  // 将打包好的文件复制到 distProd 目录
+  defConfig.plugins.push(
+    copy({
+      targets,
+    }),
+  );
+}
+else {
+  defConfig.plugins.push(
+    serve(),
+    livereload(),
+  );
 }
 
 export default defConfig;
