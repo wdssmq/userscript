977462fdd52bbd571b822f864f1c13397b8d14b8
wdssmq <wdssmq@qq.com>
2026-05-09 11:42:25 +0800

    chore: 移除不必要的导入变量，简化代码

--- packages/bilibili/src/main.js
+++ packages/bilibili/src/main.js
@@ -1,17 +1,17 @@
 // 番剧链接改为我的追番
-import _2bangumi from "./_2bangumi";
+import "./_2bangumi";
 
 // B 币领取提醒
-import _bcoin from "./_bcoin";
+import "./_bcoin";
 
 // 获取封面大图
-import _cover from "./_cover";
+import "./_cover";
 
 // 关注列表增强
-import _follow from "./_follow";
+import "./_follow";
 
 // 稍后再看导出为 *.url
-import _later2url from "./_later2url";
+import "./_later2url";
 
 // 时间轴书签
-import _time from "./_time";
+import "./_time";
